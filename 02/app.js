a55fcf1ade12a28515bfbbdde7b9d17c66500e75
Sandra <sandra.mstowskaa@gmail.com>
2025-08-12 14:38:56 +0200

    complete task 02

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,23 @@
 const x = 5;
 let result = 0;
+
+for(let i =0; i <= x; i++) {
+    result += i;
+    console.log(result);
+}
+
+console.log("Wynik:", result);
+
+
+
+const xy = 5;
+let sum = 0;
+
+let j =0;
+while(j <= xy) {
+    sum += j;
+    console.log(sum);
+    j++;
+}
+
+console.log("Wynik z pętli while:", sum);
