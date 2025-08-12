c5eec900a1fd5764c75ab6f30b80fb76a29fface
Sandra <sandra.mstowskaa@gmail.com>
2025-08-12 14:54:53 +0200

    complete task 03- complete all tasks

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,13 @@
+
 const x = 10;
 let iteration = 0;
 let randomNumber = -1;
+
+while(x !== randomNumber) {
+    randomNumber = Math.round(Math.random() * 25);
+    iteration++;
+    console.log("Wylosowany numer:", randomNumber);
+    console.log("Poszukiwany numer:", x);
+}
+
+console.log("Potrzebowałeś", iteration, "prób");
