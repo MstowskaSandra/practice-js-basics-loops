1d027b0ce9be778a17983041042cf045e9092f28
Sandra <sandra.mstowskaa@gmail.com>
2025-08-12 14:01:17 +0200

    complete task 01

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,10 @@
-const randomNumber = Math.round(Math.random() * 20);
-console.log(randomNumber);
+const randomNumber = Math.round(Math.random() * 21);
+console.log("Wylosowana liczba:", randomNumber);
+
+  if (randomNumber < 5) {
+    console.log("Wylosowana liczba jest za mała, aby użyć pętli");
+  } else {
+        for (let i = 5; i <= randomNumber; i++) {
+            console.log(i)
+        }
+  }
